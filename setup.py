9344d8ef6992e24dcc15029de4643f77569cf7a4
Jaden <jaden.pieper@nist.gov>
2021-01-05 11:18:00 -0700

    Include audio_check.wav in package

--- setup.py
+++ setup.py
@@ -15,13 +15,15 @@ setuptools.setup(
     url="https://github.com/usnistgov/mrt",
     packages=setuptools.find_packages(),
     use_scm_version={'write_to' : 'mrt/version.py'},
+    setup_requires = ['setuptools_scm'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
         "Operating System :: OS Independent",
     ],
+    include_package_data = True,
+    package_data={'':['audio_check.wav']},
     install_requires=[
-        'tkinter',
         'numpy',
         'scipy',
         'sounddevice',
